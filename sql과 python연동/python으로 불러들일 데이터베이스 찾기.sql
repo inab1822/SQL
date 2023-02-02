864f3e37f136275ae8775be0c69b8fb01509d222
김경섭 <116131859+inab1822@users.noreply.github.com>
2023-02-02 18:09:38 +0900

    Add files via upload

---
+++ sql과 python연동/python으로 불러들일 데이터베이스 찾기.sql
@@ -0,0 +1,22 @@
+show databases;
+
+use classicmodels;
+
+show tables;
+
+show full columns from customers;
+
+select * from customers;
+
+select customernumber, customername, city, country from customers;
+
+select * from customers,orders;
+
+use world;
+
+show tables;
+
+select * from country;
+
+select name, continent, region, population, gnp, gnpold, governmentform
+from country;
